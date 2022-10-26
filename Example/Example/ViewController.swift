bd8cf55306623652881d865a5c3e9effaa300ff8
vitelab <vite@sdtv.cn>
2022-10-26 15:54:15 +0800

    update framework

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by 王叶庆 on 2022/10/26.
+//
+
+import UIKit
+import NotifyStat
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        NotifyStat.shared.send(articleID: "135", title: "hello notify stat", action: "pv", publishDate: Date()) { result in
+            print(result)
+        }
+    }
+
+
+}
+
