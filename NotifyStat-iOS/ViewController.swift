21aaf184b4db8d5b910403c156e58254aea65a3d
WYQ <wyqpadding@gmail.com>
2022-10-26 12:04:00 +0800

    init

---
+++ NotifyStat-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotifyStat-iOS
+//
+//  Created by 王叶庆 on 2022/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NotifyStat-iOSUITests/NotifyStat_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NotifyStat_iOSUITestsLaunchTests.swift
+//  NotifyStat-iOSUITests
+//
+//  Created by 王叶庆 on 2022/10/26.
+//
+
+import XCTest
+
+final class NotifyStat_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
